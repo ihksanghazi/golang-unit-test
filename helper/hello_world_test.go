90aa08f42f5d0562f0ca97c7173524fd2746d645
Nursandy Ihksan <nursandyihksan97@gmail.com>
2022-12-31 21:07:15 +0700

    error & fatal

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,7 +9,7 @@ func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("sandy")
 	if result != "Hello sandy" {
 		// unit test failed
-		t.Fail()
+		t.Error("Must be 'Hello sandy'")
 	}
 	fmt.Println("TestHelloWorld Done")
 }
@@ -18,7 +18,7 @@ func TestHelloWorldIhksan(t *testing.T) {
 	result := HelloWorld("ihksan")
 	if result != "Hello ihksan" {
 		// unit test failed
-		t.FailNow()
+		t.Fatal("Must be 'Hello ihksan'")
 	}
 	fmt.Println("TestHelloWorldIhksan Done")
 }
